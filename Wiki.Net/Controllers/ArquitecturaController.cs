52b400e629706b02429c61704b3b656fb1556c29
Nicolas Velazco <nicolasvelazco@NB-NVelazco.local>
2018-08-05 14:35:20 -0300

    Se agrego el controller de Arquitectura

---
+++ Wiki.Net/Controllers/ArquitecturaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Net.Controllers
+{
+    public class ArquitecturaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
